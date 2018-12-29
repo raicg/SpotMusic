c761cb480cb8f136bdb4528a60798fd8f1a13d75
Rai Gondim <raicg2@gmail.com>
2018-12-29 20:19:36 -0300

    creating controller dashboard(home logged page), route done

--- spotmusic/config/routes.rb
+++ spotmusic/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users
-  root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+  
+  unauthenticated :user do
+    root to: "home#index"
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
