ca884192dccf23a1965274cab06d0a542014d683
Rai Gondim <raicg2@gmail.com>
2018-12-31 20:11:20 -0300

    routes to search page done.

--- spotmusic/config/routes.rb
+++ spotmusic/config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
+    resources :search, only: [:index, :new], as: :searches
   end
   
   unauthenticated :user do
-    root to: "home#index"
+    root to: "home#index", as: :root
   end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
