5af6903190719df3fe2676b49f901df7e25e9035
Rai Gondim <raicg2@gmail.com>
2019-01-03 00:33:39 -0300

    fixed search controller.

--- spotmusic/app/controllers/search_controller.rb
+++ spotmusic/app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @songs = Song.where("title LIKE ?", "%#{params[:search]}%")
     @albums = Album.where("title LIKE ?", "%#{params[:search]}%")
-    @artists = Artist.where("title LIKE ?", "%#{params[:search]}%")
+    @artists = Artist.where("name LIKE ?", "%#{params[:search]}%")
   end
 
   def new
