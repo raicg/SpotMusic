375b267d9abc98a9e508011f58b83a040438f4d7
Rai Gondim <raicg2@gmail.com>
2019-01-04 09:25:20 -0300

    Created: controller category and route to it.

--- spotmusic/config/routes.rb
+++ spotmusic/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
+    resources :caregories, only: :index
   end
   
   unauthenticated :user do
