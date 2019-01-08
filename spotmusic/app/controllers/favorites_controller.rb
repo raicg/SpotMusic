3e9d53b3915403214bb4b2913f8668869168069c
Rai Gondim <raicg2@gmail.com>
2019-01-08 10:06:23 -0300

    def Index of Favorite Controller done, Favorite Index page done.

--- spotmusic/app/controllers/favorites_controller.rb
+++ spotmusic/app/controllers/favorites_controller.rb
@@ -1,5 +1,8 @@
 class FavoritesController < ApplicationController
   def index
+    @favorite_artists = current_user.favorite.where(favoritable_type: "Artist").map(&:favoritable)
+    @favorite_albums = current_user.favorite.where(favoritable_type: "Album").map(&:favoritable)
+    @favorite_songs = current_user.favorite.where(favoritable_type: "Song").map(&:favoritable)
   end
 
   def new
