6095c4ca1c4f48a163a3219bd64bea454f87e8f6
Rai Gondim <raicg2@gmail.com>
2019-01-08 10:13:58 -0300

    Favorites Controller done, Favorites Helper done.

--- spotmusic/app/controllers/favorites_controller.rb
+++ spotmusic/app/controllers/favorites_controller.rb
@@ -6,8 +6,13 @@ class FavoritesController < ApplicationController
   end
 
   def new
+    @favoritable = current_user.favorite.new(favoritable_type: params[:favoritable_type], favoritable_id: params[:id])
+    @favoritable.save
   end
 
   def delete
+    @favoritable = current_user.favorite.find_by(favoritable_type: params[:favoritable_type], favoritable_id: params[:id])
+    @favoritable.destroy
   end
+
 end
